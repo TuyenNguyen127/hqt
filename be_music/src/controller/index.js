84546b4f78963750e36ff713c632a989f086598a
neihyud <shinestarb2@gmail.com>
2023-04-05 14:00:52 +0700

    up2

---
+++ be_music/src/controller/index.js
@@ -0,0 +1,3 @@
+exports.favorire = (req, res) => {
+    console.log("")
+}
---
+++ be_music/src/model/Album.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const AlbumSchema = new Schema({
+    name: String,
+    artist: {
+        type: [mongoose.Types.ObjectId],
+        ref: 'Artist',
+    },
+    song: {
+        type: [mongoose.Types.ObjectId],
+        ref: 'Song',
+    },
+
+    description: String,
+    thumbnail: {
+        type: String,
+        default: require('../../assets/img/nord3.png'),
+    },
+    num_liked: Number,
+    num_shared: Number,
+})
+
+module.exports = mongoose.model('Album', AlbumSchema)
